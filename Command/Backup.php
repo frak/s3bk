7e31f3aad7d0e6e92bc85dff1bd07417b3fda1e7
Michael Davey <frak.off@gmail.com>
2011-12-30 13:16:21 +0000

    Progress bar support added

--- Command/Backup.php
+++ Command/Backup.php
@@ -4,54 +4,105 @@ namespace Command;
 
 class Backup extends \Core\Command
 {
+    private $_systemFiles = array('.DS_Store', 'Thumbs.db');
+
+    private $_progressBar;
+
+    private $_amountDone = 0;
+
     public function run()
     {
+        $this->_progressBar = $this->_getKey('pbar');
+        $this->_progressBar->UPDATED = false;
         if(!$this->_s3->if_bucket_exists($this->_getBucketName())) {
             \Core\Bucket::create($this->_s3, $this->_getBucketName());
         }
+        $this->_s3->register_streaming_read_callback(array($this, '_writeCallback'));
         $base = $this->_getKey('path');
         $iter = new \RecursiveIteratorIterator(new \RecursiveDirectoryIterator($base));
         while($iter->valid()) {
             $key = $iter->key();
-//            if(!$iter->isDot() && !isFileIgnored($key) && !isPatternIgnored($key)) {
-            if(!$iter->isDot()) {
-                $this->_processFile($base, $key);
-//                $progressBar->UPDATED = false;
+            if(!$iter->isDot() && !$this->_isSystemFile($key)) {
+                $res = $this->_processFile($base, $key);
+                if(!$res) {
+                    echo "Problems with AmazonS3 service - please try again in a few minutes\n";
+                    return;
+                }
+                $this->_progressBar->UPDATED = false;
             }
             $iter->next();
         }
+        $this->_prune($base);
+    }
+
+    private function _isSystemFile($fileName) {
+        $fileName = explode(DIRECTORY_SEPARATOR, $fileName);
+        $fileName = array_pop($fileName);
+        return in_array($fileName, $this->_systemFiles);
+    }
+
+    public function _writeCallback($curlHandle, $fileHandle, $length)
+    {
+        if(!$this->_progressBar->UPDATED) {
+            $this->_amountDone = curl_getinfo($curlHandle, CURLINFO_CONTENT_LENGTH_UPLOAD);
+            $this->_progressBar->reset('%fraction% KB [%bar%] %percent%', '=>', ' ', 100, $this->_amountDone);
+            $this->_progressBar->UPDATED = true;
+        }
+        $this->_progressBar->update(curl_getinfo($curlHandle, CURLINFO_SIZE_UPLOAD));
     }
 
     private function _processFile($base, $name)
     {
-        $bucket = $this->_getBucketName();
-        $fileName = str_replace($base, '', $name);
-        echo "{$fileName}... ";
-        $sourceFile = $name;
-        if($this->_s3->if_object_exists($bucket, $fileName)) {
-            do {
-                $res = $this->_s3->get_object_headers($bucket, $fileName);
-            } while(!$res->isOK());
-            $remoteMd5 = str_replace('"', '', $res->header['etag']);
-            $localMd5 = md5_file($sourceFile);
-            if($remoteMd5 === $localMd5) {
-                echo "unchanged" . PHP_EOL;
+        try {
+            $bucket = $this->_getBucketName();
+            $fileName = str_replace($base . DIRECTORY_SEPARATOR, '', $name);
+            echo "{$fileName}... ";
+            $sourceFile = $name;
+            if($this->_s3->if_object_exists($bucket, $fileName)) {
+                do {
+                    $res = $this->_s3->get_object_headers($bucket, $fileName);
+                } while(!$res->isOK());
+                $remoteMd5 = str_replace('"', '', $res->header['etag']);
+                $localMd5 = md5_file($sourceFile);
+                if($remoteMd5 === $localMd5) {
+                    echo "unchanged" . PHP_EOL;
+                } else {
+                    echo "modified" . PHP_EOL;
+                    $this->_s3->delete_object($bucket, $fileName);
+                    $res = $this->_s3->create_object($bucket, $fileName, array(
+                        'fileUpload' => $sourceFile,
+                    ));
+                    $this->_progressBar->update($this->_amountDone);
+                    echo PHP_EOL;
+                }
             } else {
-                echo "modified" . PHP_EOL;
-                $this->_s3->delete_object($bucket, $fileName);
+                echo "created" . PHP_EOL;
                 $res = $this->_s3->create_object($bucket, $fileName, array(
                     'fileUpload' => $sourceFile,
                 ));
-                // $progressBar->update($allDone);
+                $this->_progressBar->update($this->_amountDone);
                 echo PHP_EOL;
             }
-        } else {
-            echo "uploading" . PHP_EOL;
-            $res = $this->_s3->create_object($bucket, $fileName, array(
-                'fileUpload' => $sourceFile,
-            ));
-            // $progressBar->update($allDone);
-            echo PHP_EOL;
+            return true;
+        } catch (RequestCore_Exception $e) {
+            return false;
         }
     }
+
+    private function _prune($base) {
+        $bucket = $this->_getBucketName();
+        $list = $this->_s3->get_object_list($bucket);
+        $delCount = 0;
+        foreach($list as $file) {
+            if(!file_exists($base . DIRECTORY_SEPARATOR . $file)) {
+                $this->_s3->batch()->delete_object($bucket, $file);
+                ++$delCount;
+            }
+        }
+        if($delCount > 0) {
+            $this->_s3->batch()->send();
+        }
+        echo "Deleted {$delCount} file(s)" . PHP_EOL;
+    }
+
 }
