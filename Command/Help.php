b82dc5ac923359ba30e4e015ef42f6357bf31b8b
Michael Davey <frak.off@gmail.com>
2011-12-30 23:58:03 +0000

    Added help text

--- Command/Help.php
+++ Command/Help.php
@@ -7,25 +7,27 @@ class Help extends \Core\Command
     private $_commands = array(
         'mount' => array(
             'short' => 'Maps a local drive to S3',
-            'full' => "\nThe first parameter needs to be the path to the local directory that you want to mount,\nand the second parameter should be the name you want to give it.\n"
+            'full'  => "The first parameter needs to be the path to the local directory that you want to mount,\nand the second parameter should be the name you want to give it.\n"
         ),
         'list' => array(
             'short' => 'List mapped directories',
-            'full' => "\nWill show a list of currently mounted directories and their type.\n"
-
-
+            'full'  => "Will show a list of currently mounted directories and their type.\n"
         ),
         'remove' => array(
             'short' => 'Remove a local drive mapping',
-            'full' => "\nYou should specify the mount point that you wish to remove as the first parameter.\nSpecify a second parameter of 'force' if you wish to force the removal of remaining files."
+            'full'  => "You should specify the mount point that you wish to remove as the first parameter.\nSpecify a second parameter of 'force' if you wish to force the removal of remaining files."
         ),
         'backup' => array(
             'short' => 'Synchronise files with S3',
-            'full' => "\nYou should specify the mount point that you wish to synchronise with S3 as the first\nparameter.\n"
+            'full'  => "You should specify the mount point that you wish to synchronise with S3 as the first\nparameter.\n"
         ),
         'restore' => array(
             'short' => 'Restore files to the mount point from S3',
-            'full' => "\nYou should specify the mount point that you wish to restore from as the first parameter.\n"
+            'full'  => "You should specify the mount point that you wish to restore from as the first parameter.\n"
+        ),
+        'interval' => array(
+            'short' => 'Schedule backups at intervals',
+            'full'  => "Specify the mount point name and then the interval between backups, either in minutes alone (30)\nor with hours (2h5m). To stop scheduled backups, use the interval 'clear'"
         ),
     );
 
